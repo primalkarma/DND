1320b990a6552b5720c0d12fc6263d42e8b7e620
primalkarma <primalkarma@gmail.com>
2025-07-04 18:31:11 +0530

    Initial Commit

---
+++ DND/DNDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNDApp.swift
+//  DND
+//
+//  Created by Parimal Devi on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DNDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
